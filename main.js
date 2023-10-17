637538a153e4609278c29244a86fcdfa84701e14
NJ-Khan <127430431+NJ-Khan@users.noreply.github.com>
2023-10-17 21:16:28 +0430

    Update main.js
    swiper places swaped

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-// Header Scroll 
-let nav = document.querySelector(".navbar");
-window.onscroll = function() {
-    if(document.documentElement.scrollTop > 100){
-        nav.classList.add("header-scrolled");
-    }else{
-        nav.classList.remove("header-scrolled");
-    }
-}
 
-// nav hide  
-let navBar = document.querySelectorAll(".nav-link");
-let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function(a){
-    a.addEventListener("click", function(){
-        navCollapse.classList.remove("show");
-    })
-})
 /*=============== SWIPER JS ===============*/
 let swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
@@ -48,3 +31,23 @@ let swiper = new Swiper(".slide-content", {
     },
 
   });
+
+
+// Header Scroll 
+let nav = document.querySelector(".navbar");
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 100){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide  
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
